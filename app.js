9ca8d336713fbdb5882ffbb60287b5bc2e621d56
monsterseason33-ship-it <monsterseason33@gmail.com>
2026-08-08 12:50:21 +0700

    Add key SVG icon to password row in subscription details

--- app.js
+++ app.js
@@ -843,7 +843,7 @@ function renderSubscriptions() {
 
           <div class="credential-row">
             <div class="credential-label">
-              <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="var(--gold-light)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.778-7.778zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4"></path></svg>
+              <svg width="13" height="13" viewBox="0 0 24 24" fill="none" stroke="var(--gold-light, #fde047)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 2l-2 2m-7.61 7.61a5.5 5.5 0 1 1-7.778 7.778 5.5 5.5 0 0 1 7.778-7.778zm0 0L15.5 7.5m0 0l3 3L22 7l-3-3m-3.5 3.5L19 4"></path></svg>
               <span>รหัสผ่าน:</span>
             </div>
             <div class="credential-val-box">
